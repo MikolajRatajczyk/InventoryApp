f9d5c6cc934f6595a8b6a87a6d710f45a111b702
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-05-07 18:53:54 +0200

    Add to ProductEditiActivity ability to load selected product

--- app/src/main/java/com/ratajczykdev/inventoryapp/ProductEditActivity.java
+++ app/src/main/java/com/ratajczykdev/inventoryapp/ProductEditActivity.java
@@ -1,16 +1,92 @@
 package com.ratajczykdev.inventoryapp;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-//  TODO: fill
-public class ProductEditActivity extends AppCompatActivity
+import com.ratajczykdev.inventoryapp.data.ProductContract.ProductEntry;
+
+//  TODO: finish
+public class ProductEditActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>
 {
+    /**
+     * Add photo button
+     */
+    private Button buttonAddPhoto;
+
+    /**
+     * Name EditText
+     */
+    private EditText editTextName;
+
+    /**
+     * Quantity EditText
+     */
+    private EditText editTextQuantity;
+
+    /**
+     * Price EditText
+     */
+    private EditText editTextPrice;
+
+    /**
+     * Delete button
+     */
+    private Button buttonDelete;
+
+    /**
+     * Content URI for the existing product
+     */
+    private Uri currentProductUri;
+
+    private static final int EDITED_PRODUCT_LOADER_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_edit);
+
+        if (getIntent() != null)
+        {
+            currentProductUri = getIntent().getData();
+            getLoaderManager().initLoader(EDITED_PRODUCT_LOADER_ID, null, this);
+        }
+        //  TODO: finish whole editing behaviour
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle)
+    {
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_PHOTO,
+                ProductEntry.COLUMN_PRODUCT_PRICE,
+                ProductEntry.COLUMN_PRODUCT_QUANTITY};
+
+        return new CursorLoader(this, currentProductUri, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor)
+    {
+        //  TODO: fill
+        //  TODO: delete debugging Toast
+        Toast.makeText(this, currentProductUri.toString() + "->" + cursor.toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader)
+    {
+        //  TODO: fill
     }
 }
