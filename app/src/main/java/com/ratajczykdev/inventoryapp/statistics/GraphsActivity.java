986546373e9fa39ddf06b9437ee2bca44ce17579
MikolajRatajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-06-30 17:27:38 +0200

    Add GraphsActivity

---
+++ app/src/main/java/com/ratajczykdev/inventoryapp/statistics/GraphsActivity.java
@@ -0,0 +1,17 @@
+package com.ratajczykdev.inventoryapp.statistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ratajczykdev.inventoryapp.R;
+
+public class GraphsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphs);
+    }
+}
