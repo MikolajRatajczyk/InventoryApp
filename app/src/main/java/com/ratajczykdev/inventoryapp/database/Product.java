0cfba7a4e8ffd6b40ece3b78e02c8febd673daeb
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-10 17:54:48 +0200

    Add Product data model class

---
+++ app/src/main/java/com/ratajczykdev/inventoryapp/database/Product.java
@@ -0,0 +1,60 @@
+package com.ratajczykdev.inventoryapp.database;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Data model class for the database
+ * <p>
+ * Represents single product and products table name
+ */
+@Entity(tableName = "products")
+public class Product {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private String photoUri;
+    private float price;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUri() {
+        return photoUri;
+    }
+
+    public void setPhotoUri(String photoUri) {
+        this.photoUri = photoUri;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
