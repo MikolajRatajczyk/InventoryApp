e5a992de491bb2e489d8159608e24b61a8083839
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-10 18:51:18 +0200

    Change ProductsDatabase class to Singleton

--- app/src/main/java/com/ratajczykdev/inventoryapp/database/ProductsDatabase.java
+++ app/src/main/java/com/ratajczykdev/inventoryapp/database/ProductsDatabase.java
@@ -3,12 +3,28 @@ package com.ratajczykdev.inventoryapp.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 /**
  * Products database abstract class
+ * <p>
+ * Uses Singleton design pattern
  */
 @Database(entities = {Product.class}, version = 1, exportSchema = false)
 public abstract class ProductsDatabase extends RoomDatabase {
 
+    private static ProductsDatabase productsDatabaseInstance;
+
     public abstract ProductDao productDao();
+
+    public static ProductsDatabase getProductsDatabaseInstance(Context context) {
+        if (productsDatabaseInstance == null) {
+            productsDatabaseInstance = Room.databaseBuilder(context, ProductsDatabase.class, "products")
+                    .fallbackToDestructiveMigration()
+                    .build();
+            return productsDatabaseInstance;
+        } else {
+            return productsDatabaseInstance;
+        }
+    }
 }
