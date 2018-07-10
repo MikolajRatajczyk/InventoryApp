27414174dec519a00fdbbdaa34f9604687009577
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-10 19:05:45 +0200

    Change products database name

--- app/src/main/java/com/ratajczykdev/inventoryapp/database/ProductsDatabase.java
+++ app/src/main/java/com/ratajczykdev/inventoryapp/database/ProductsDatabase.java
@@ -19,7 +19,7 @@ public abstract class ProductsDatabase extends RoomDatabase {
 
     public static ProductsDatabase getProductsDatabaseInstance(Context context) {
         if (productsDatabaseInstance == null) {
-            productsDatabaseInstance = Room.databaseBuilder(context, ProductsDatabase.class, "products")
+            productsDatabaseInstance = Room.databaseBuilder(context, ProductsDatabase.class, "productsDb")
                     .fallbackToDestructiveMigration()
                     .build();
             return productsDatabaseInstance;
