69b53e30cec098f6048cf09cbc4b2184320a3079
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-10 18:28:29 +0200

    Add methods

--- app/src/main/java/com/ratajczykdev/inventoryapp/database/ProductDao.java
+++ app/src/main/java/com/ratajczykdev/inventoryapp/database/ProductDao.java
@@ -4,24 +4,34 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import java.util.List;
 
+/**
+ * Interface class for database access
+ */
 @Dao
 public interface ProductDao {
 
     @Query("SELECT * FROM products")
     List<Product> getAll();
 
-    @Query("SELECT * FROM products WHERE id (:ids)")
+    @Query("SELECT * FROM products WHERE id IN (:ids)")
     List<Product> findAllByIds(int[] ids);
 
     @Query("SELECT * FROM products WHERE name LIKE :searchName LIMIT 1")
     Product findByName(String searchName);
 
+    @Query("SELECT * FROM products WHERE id = :searchId")
+    Product findById(int searchId);
+
     @Insert
-    void insertAll(Product[] products);
+    void insertMultipleProducts(List<Product> productsList);
+
+    @Update
+    void updateProduct(Product product);
 
     @Delete
-    void delete(Product product);
+    void deleteProduct(Product product);
 }
