c4854cba944a67cdc8f50f7c7a8fd092686a8ee1
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-11 14:31:59 +0200

    Add ProductRepository class

---
+++ app/src/main/java/com/ratajczykdev/inventoryapp/database/ProductRepository.java
@@ -0,0 +1,59 @@
+package com.ratajczykdev.inventoryapp.database;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+/**
+ * Abstracts access to multiple data sources (here: only one)
+ * The Repository is not part of the Architecture Components libraries,
+ * but is a suggested best practice for code separation and architecture.
+ */
+public class ProductRepository {
+    //  TODO: add more methods wrappers
+
+    private ProductDao productDao;
+    private LiveData<List<Product>> allProducts;
+
+    ProductRepository(Application application) {
+        ProductDatabase productDatabase = ProductDatabase.getProductsDatabaseInstance(application);
+        productDao = productDatabase.productDao();
+        allProducts = productDao.getAll();
+    }
+
+    /**
+     * Wrapper for getAll()
+     */
+    LiveData<List<Product>> getAll() {
+        return allProducts;
+    }
+
+    /**
+     * Wrapper for insertSingle()
+     * Call this on a non-UI thread or app will crash.
+     */
+    public void insertSingle(Product product) {
+
+        new insertSingleAsyncTask(productDao).execute(product);
+    }
+
+    /**
+     * Static class for insertSingle
+     */
+    private static class insertSingleAsyncTask extends AsyncTask<Product, Void, Void> {
+        private ProductDao productDao;
+
+        insertSingleAsyncTask(ProductDao productDao) {
+            this.productDao = productDao;
+        }
+
+        @Override
+        protected Void doInBackground(Product... products) {
+            productDao.insertSingle(products[0]);
+            return null;
+        }
+    }
+
+}
