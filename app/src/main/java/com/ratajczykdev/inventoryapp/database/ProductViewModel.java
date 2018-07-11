f502613512657b29c71ca406e350f055ef82b289
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-11 14:39:56 +0200

    Add ProductViewModel class

---
+++ app/src/main/java/com/ratajczykdev/inventoryapp/database/ProductViewModel.java
@@ -0,0 +1,38 @@
+package com.ratajczykdev.inventoryapp.database;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+/**
+ * Provides data to the UI and survive configuration changes
+ */
+public class ProductViewModel extends AndroidViewModel {
+
+    private ProductRepository productRepository;
+    private LiveData<List<Product>> allProducts;
+
+    public ProductViewModel(@NonNull Application application) {
+        super(application);
+        productRepository = new ProductRepository(application);
+        allProducts = productRepository.getAll();
+    }
+
+    /**
+     * Completely hides implementation from the UI
+     */
+    public LiveData<List<Product>> getAll() {
+        //  TODO: change method name to getAllProducts (and previous classes)
+        return allProducts;
+    }
+
+    /**
+     * This way implementation of insertSingle() is completely hidden from UI
+     */
+    public void insertSingle(Product product) {
+        productRepository.insertSingle(product);
+    }
+}
