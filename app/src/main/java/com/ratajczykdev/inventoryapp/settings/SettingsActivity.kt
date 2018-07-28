7eba048a0eb49a73a0c0c65a4b7af08a99f3eb2f
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-28 17:00:48 +0200

    Add SettingsActivity written in Kotlin

---
+++ app/src/main/java/com/ratajczykdev/inventoryapp/settings/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.ratajczykdev.inventoryapp.settings
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ratajczykdev.inventoryapp.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
