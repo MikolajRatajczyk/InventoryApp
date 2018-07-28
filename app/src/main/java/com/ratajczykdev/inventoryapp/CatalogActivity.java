fb6a7ea0e0911210453977298d7954a772068688
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-28 17:28:01 +0200

    Add option to start SettingsActivity
    Only for testing, temporary

--- app/src/main/java/com/ratajczykdev/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/ratajczykdev/inventoryapp/CatalogActivity.java
@@ -20,6 +20,7 @@ import android.view.animation.AnimationUtils;
 
 import com.ratajczykdev.inventoryapp.database.Product;
 import com.ratajczykdev.inventoryapp.database.ProductViewModel;
+import com.ratajczykdev.inventoryapp.settings.SettingsActivity;
 import com.ratajczykdev.inventoryapp.statistics.StatisticsActivity;
 
 import java.util.List;
@@ -193,6 +194,10 @@ public class CatalogActivity extends AppCompatActivity {
             //  to start content transition in AboutActivity
             Bundle bundle = ActivityOptions.makeSceneTransitionAnimation(CatalogActivity.this).toBundle();
             startActivity(intent, bundle);
+        } else if (currentItemId == R.id.activity_catalog_appbar_actions_settings) {
+            //  TODO: delete, make new icon for settings
+            Intent intent = new Intent(CatalogActivity.this, SettingsActivity.class);
+            startActivity(intent);
         }
         return true;
     }
