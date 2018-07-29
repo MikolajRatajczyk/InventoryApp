c0128950e0aec45718f81cbaaa76c191cc9cd756
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-29 13:51:28 +0200

    Add companion object with configuring method
    Method helps to configure BottomNavigationView with listeners

--- app/src/main/java/com/ratajczykdev/inventoryapp/bottomnavigation/BottomNavigationHelper.kt
+++ app/src/main/java/com/ratajczykdev/inventoryapp/bottomnavigation/BottomNavigationHelper.kt
@@ -1,5 +1,56 @@
 package com.ratajczykdev.inventoryapp.bottomnavigation
 
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Intent
+import android.support.design.widget.BottomNavigationView
+import android.view.MenuItem
+import com.ratajczykdev.inventoryapp.AboutActivity
+import com.ratajczykdev.inventoryapp.R
+import com.ratajczykdev.inventoryapp.settings.SettingsActivity
+import com.ratajczykdev.inventoryapp.statistics.StatisticsActivity
+
+/**
+ * Helper class that configures [BottomNavigationView]
+ */
 class BottomNavigationHelper {
+
+    companion object {
+
+        /**
+         * Configures listeners
+         *
+         * @param bottomNavigationView to be configured with listeners
+         * @param currentActivity where BNV is
+         */
+        fun setBottomNaviagtionListener(bottomNavigationView: BottomNavigationView, currentActivity: Activity) {
+            bottomNavigationView.setOnNavigationItemSelectedListener { currentItem: MenuItem ->
+                when (currentItem.itemId) {
+                    R.id.settings_button -> startSettingsActivity(currentActivity)
+                    R.id.statistics_button -> startStatisticsActivity(currentActivity)
+                    R.id.about_button -> startAboutActivity(currentActivity)
+                }
+                true
+            }
+        }
+
+        private fun startSettingsActivity(currentActivity: Activity) {
+            val intent = Intent(currentActivity, SettingsActivity::class.java)
+            currentActivity.startActivity(intent)
+        }
+
+        private fun startStatisticsActivity(currentActivity: Activity) {
+            val intent = Intent(currentActivity, StatisticsActivity::class.java)
+            currentActivity.startActivity(intent)
+        }
+
+        private fun startAboutActivity(currentActivity: Activity) {
+            val intent = Intent(currentActivity, AboutActivity::class.java)
+            //  to start content transition in AboutActivity
+            val bundle = ActivityOptions.makeSceneTransitionAnimation(currentActivity).toBundle()
+            currentActivity.startActivity(intent, bundle)
+        }
+
+    }
 
 }
