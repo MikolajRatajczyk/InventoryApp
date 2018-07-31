57bdb3dac4a3da17826ff47976fa22a17dce9b95
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-07-31 14:30:49 +0200

    Add MainActivity

---
+++ app/src/main/java/com/ratajczykdev/inventoryapp/fragmentsui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ratajczykdev.inventoryapp.fragmentsui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ratajczykdev.inventoryapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
