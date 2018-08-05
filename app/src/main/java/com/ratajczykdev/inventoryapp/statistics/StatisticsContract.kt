6bc5c2452a0ab5afc809e0daaf05a38c2c5421f9
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-08-06 00:04:10 +0200

    Rewrite to Kotlin and update comment

---
+++ app/src/main/java/com/ratajczykdev/inventoryapp/statistics/StatisticsContract.kt
@@ -0,0 +1,16 @@
+package com.ratajczykdev.inventoryapp.statistics
+
+/**
+ * Helper class containing constants for statistics data transfer between Activities/Fragments
+ *
+ * For example: Intents
+ *
+ * @author Mikołaj Ratajczyk <mikolaj.ratajczyk></mikolaj.ratajczyk>@gmail.com>
+ */
+object StatisticsContract {
+    val STATISTICS_MAP_NAME = "STATISTICS_MAP"
+    val ITEMS_NUMBER_KEY = "ITEMS_NUMBER"
+    val PRODUCTS_NUMBER_KEY = "PRODUCTS_NUMBER"
+    val PRODUCTS_MAX_PRICE_KEY = "MAX_PRODUCTS_PRICE"
+    val PRODUCTS_MIN_PRICE_KEY = "MIN_PRODUCTS_PRICE"
+}
