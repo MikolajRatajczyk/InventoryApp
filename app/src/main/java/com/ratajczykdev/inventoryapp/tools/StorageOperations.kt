25bf463a70266accd83f20d3d8ca24bd695e3eb2
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-08-16 11:11:59 +0200

    Add helper class handling storage operations

---
+++ app/src/main/java/com/ratajczykdev/inventoryapp/tools/StorageOperations.kt
@@ -0,0 +1,53 @@
+package com.ratajczykdev.inventoryapp.tools
+
+import android.content.Context
+import java.io.File
+import java.io.FileOutputStream
+
+/**
+ * Helper class that handles storage operations e.g. writing to files, creating directories
+ */
+object StorageOperations {
+
+    /**
+     * Create directory in primary shared/external storage device (app folder)
+     *
+     * @param directoryName name for new directory
+     * @param context context
+     * @return [File] object representing newly created or already existing directory,
+     * if failed returns non-existing directory
+     */
+    fun createDirInExternal(directoryName: String, context: Context?): File {
+        //  type of files directory - null for the root of the files directory
+        val TYPE_FOR_ROOT_DIRECTORY = null
+        //  get external storage path
+        val externalPath = context?.getExternalFilesDir(TYPE_FOR_ROOT_DIRECTORY)
+
+        val newDirectoryPath = File(externalPath, directoryName)
+        if (!newDirectoryPath.exists()) {
+            if (!newDirectoryPath.mkdirs()) {
+                val nonExistingDirectory = File("")
+                return nonExistingDirectory
+            }
+        }
+        return newDirectoryPath
+    }
+
+    /**
+     * Writes [String] to file
+     */
+    fun writeStringToFile(directoryPath: File, fileName: String, fileContent: String) {
+        if (directoryPath.exists()) {
+            //  create file
+            val file = File(directoryPath, fileName)
+            //  create stream for file
+            val stream = FileOutputStream(file)
+            //  write String to file
+            try {
+                stream.write(fileContent.toByteArray())
+            } finally {
+                stream.close()
+            }
+        }
+    }
+}
