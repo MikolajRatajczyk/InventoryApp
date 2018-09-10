21ff86e8ff7ac8fdb1f63a9ba546aa1f92a385be
Mikolaj Ratajczyk <37159921+MikolajRatajczyk@users.noreply.github.com>
2018-09-10 10:40:42 +0200

    Add new Kotlin Activity to replace old Java one

---
+++ app/src/main/java/com/ratajczykdev/inventoryapp/detailandedit/ProductDetailKotlinActivity.kt
@@ -0,0 +1,13 @@
+package com.ratajczykdev.inventoryapp.detailandedit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ratajczykdev.inventoryapp.R
+
+class ProductDetailKotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail_kotlin)
+    }
+}
